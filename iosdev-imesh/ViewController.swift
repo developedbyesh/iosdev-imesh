4827f151e2f16045798edf2f2a47ffb18fba8ece
Imesh Gunasekara <37379918+eshwanted@users.noreply.github.com>
2019-05-21 11:08:18 +0530

    Feature/firebase auth (#1)
    * firebase authentication and login screen
    
    * Firebase authentication login button

--- iosdev-imesh/ViewController.swift
+++ iosdev-imesh/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        // Get the default auth UI object
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+        // Log the error
+            return
+        }
+        
+        // Set ourselves as the delegate
+        authUI?.delegate = self
+        
+        // Get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // Show it
+        present(authViewController, animated: true, completion: nil)
+    }
+    
 }
+
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        // Check if there was an Error
+        if error != nil {
+            // Log the error
+            return
+        }
+        // authDataResult?.user.uid
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+    
+    
+   }
 
